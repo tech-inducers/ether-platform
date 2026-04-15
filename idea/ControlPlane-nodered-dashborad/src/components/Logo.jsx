d3dd573858f7773837bb7dff7101a19b1545351e
soumyaraja <raj.soumya@gmail.com>
2026-04-16 00:54:22 +0100

    moved dashboard and dropped agent framework

--- idea/ControlPlane-nodered-dashborad/src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function Logo({ size = 30 }) {
-  return <img src="/logo.png" alt="Ether" 
-    style={{ width:size, height:size, borderRadius:size*0.23, display:'block' }}/>;
-}
